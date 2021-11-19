0700701a0707fd6564ac4f996710a21c078a85af
NeSh74 <nejdie@yahoo.com>
2021-11-19 20:08:10 +0200

    add

---
+++ 09. XML-Processing/09. XML-Processing-Exercises/Solutions/09. Import Suppliers/CarDealer/DTO/ImportDto/ImportSupplierDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTO.ImportDto
+{
+    [XmlType("Supplier")]
+
+    public class ImportSupplierDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("IsImporter")]
+        public bool IsImporter { get; set; }
+    }
+}
